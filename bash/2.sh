864006537f52d37fecc9e3ea55a9044094e9bfa2
Sergey <88794938+gltexture@users.noreply.github.com>
2025-03-21 18:21:22 +0300

    Update 2.sh

--- bash/2.sh
+++ bash/2.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+file=$1
+dirToWrite=$2
+
+if ! test -d $file ; then
+	echo "File ${file} doesn't exist";
+	exit 1
+fi
+
+if ! test -d $dirToWrite ; then
+        echo "File ${dirToWrite} doesn't exist";
+        exit 1
+fi    
+
+find $file -maxdepth 1 ! -name $file -type d -exec basename {} \; | while read saved;
+	do
+		filename=$2/$saved.txt;
+		touch $filename;
+		echo $(ls $1/$saved | wc -l) > $filename;
+		echo $(ls $1/$saved | wc -l);
+	done
