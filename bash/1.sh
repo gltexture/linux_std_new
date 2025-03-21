6d9dfb66c7d5c3e0291e0c0cf2ddf21473275a73
Sergey <88794938+gltexture@users.noreply.github.com>
2025-03-21 18:20:59 +0300

    Update 1.sh

--- bash/1.sh
+++ bash/1.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+timeframe=$1
+file=$2
+
+while test timeframe > 0 ;
+do
+	loadavg=$(cat /proc/loadavg)
+	dt=$(date +%Y-%H-%M-%S)
+	echo "$dt $loadavg" >> ${file} && echo "$dt $loadavg"
+	timeframe=$((timeframe - 1))
+	sleep 1
+done
