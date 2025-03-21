9cc7660c9385db5138ce50281c23f1c8baf6cee5
Sergey <88794938+gltexture@users.noreply.github.com>
2025-03-21 18:21:48 +0300

    Update 3.sh

--- bash/3.sh
+++ bash/3.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+
+files=$@
+
+for file in "$@";
+	do
+		if test -f $file ; then
+			strokes=$(cat $file | wc -l);
+			echo "$file -> $strokes";
+		else
+			echo "Not found or not a file: $file"
+		fi
+	done
