bc6a01af0f15cf1a33996a965c71e409c29b809f
Sergey <88794938+gltexture@users.noreply.github.com>
2025-04-03 10:31:43 +0300

    Update 3.sh

--- bash/3.sh
+++ bash/3.sh
@@ -4,11 +4,11 @@
 files=$@
 
 for file in "$@";
-	do
-		if test -f $file ; then
-			strokes=$(cat $file | wc -l);
-			echo "$file -> $strokes";
-		else
-			echo "Not found or not a file: $file"
-		fi
-	done
+        do
+                if test -f $file ; then
+                        strokes=$(cat $file | wc -l);
+                        echo "$file -> $strokes";
+                else
+                        echo "$file does not exist"
+                fi
+        done
