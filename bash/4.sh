5aebe5d5d552d15f48a5ee07c0ff64eee127aa15
Sergey <88794938+gltexture@users.noreply.github.com>
2025-04-03 11:17:28 +0300

    Update 4.sh

--- bash/4.sh
+++ bash/4.sh
@@ -3,15 +3,15 @@
 planet=$1
 
 case $planet in
-        "Mercury") echo "0" ;;
+        "Mercury") echo "0" exit 1 ;;
         "Venus") echo "0" ;;
         "Earth") echo "1" ;;
         "Mars") echo "2" ;;
-        "Jupiter") echo "98" ;;     
+        "Jupiter") echo "98" ;;
         "Saturn") echo "83" ;;
         "Uranus") echo "27" ;;
         "Neptune") echo "14" ;;
-        *) 
+        *)
                 echo "Unknown planet" 
                 exit 1
                 ;;
