cbf22c7f0680a7a122f6b0b972055b654d0690ed
Sergey <88794938+gltexture@users.noreply.github.com>
2025-04-03 11:04:20 +0300

    Update 3.sh

--- bash/3.sh
+++ bash/3.sh
@@ -7,7 +7,7 @@ for file in "$@";
         do
                 if test -f $file ; then
                         strokes=$(cat $file | wc -l);
-                        echo "$file -> $strokes";
+                        echo "$file => $strokes";
                 else
                         echo "$file => not found"
                 fi
