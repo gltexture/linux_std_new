1b31a2444e10db45b6a469be402e8b4e12a0c27b
Sergey <88794938+gltexture@users.noreply.github.com>
2025-04-05 09:45:01 +0300

    Update 2.sh

--- bash/2.sh
+++ bash/2.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-file=$1
-dirToWrite=$2
-
-if ! test -d $file ; then
-        echo "256";
-        exit 1
+if [ ! -e "$1" ]; then
+    echo "256"
+    exit 1
 fi
 
-if ! test -d $dirToWrite ; then
-        echo "256";
-        exit 1
-fi
-
-find $file -maxdepth 1 ! -name $file -type d -exec basename {} \; | while read saved;
-        do
-                filename=$2/$saved.txt;
-                touch $filename;
-                echo "$(ls $1/$saved | wc -l)" >> $filename;
-        done
+for dir in "$1"/*/; do
+    if [ -d "$dir" ]; then
+        count=$(find $dir -maxdepth 1 | wc -l)
+        echo "$count" >> $dir
+    fi
+done
