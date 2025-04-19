2aae03f7c5bbd6a9e664dbd011d9d9571f45c791
Sergey <88794938+gltexture@users.noreply.github.com>
2025-04-19 10:42:07 +0300

    Update 2.sh

--- bash/2.sh
+++ bash/2.sh
@@ -1,18 +1,1 @@
-#!/bin/bash
 
-directory=$1
-
-if [ ! -d "$directory" ]; then
-    echo "Error: Directory '$directory' does not exist."
-    exit 1
-fi
-
-for subdir in "$directory"
-do
-    if [ -d "$subdir" ];
-  then
-        subdir_name=$(basename "$subdir")
-        count=$(find "$subdir" -mindepth 1 -maxdepth 1 | wc -l)
-        echo "$count" > "$directory/$subdir_name.txt"
-    fi    
-done  
