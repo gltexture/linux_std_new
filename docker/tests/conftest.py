671e8840547b56b09168df311bdeceda8de9a1f2
Sergey <88794938+gltexture@users.noreply.github.com>
2025-05-24 11:46:17 +0300

    Update conftest.py

--- docker/tests/conftest.py
+++ docker/tests/conftest.py
@@ -1,64 +1,40 @@
-import pytest
-import os
+import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.database import AsyncSessionLocal, init_db as create_tables
 from src.main import app
 from src.models import User
 
-# Синхронное подключение для тестов
-SYNC_DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://kubsu:kubsu@localhost:5432/kubsu_test"
-)
 
-# Асинхронное подключение (если нужно)
-ASYNC_DATABASE_URL = SYNC_DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
+@pytest_asyncio.fixture(scope="session")
+async def init_db() -> None:
+    await create_tables()
 
-# Синхронный движок для фикстур
-test_engine = create_engine(SYNC_DATABASE_URL)
-TestSessionLocal = sessionmaker(
-    bind=test_engine,
-    expire_on_commit=False
-)
 
-@pytest.fixture(scope="session")
-def init_db():
-    with test_engine.begin() as conn:
-        User.metadata.create_all(conn)
-    yield
-    with test_engine.begin() as conn:
-        User.metadata.drop_all(conn)
-
-@pytest.fixture(scope="function")
-def db():
-    session = TestSessionLocal()
-    try:
+@pytest_asyncio.fixture(scope='function')
+async def db() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
         yield session
-    finally:
-        session.close()
+
 
 @pytest_asyncio.fixture(autouse=True)
 async def test_client():
-    async with AsyncClient(
-        transport=ASGITransport(app=app),
-        base_url="http://test",
-        follow_redirects=True
-    ) as client:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
         yield client
 
-@pytest.fixture(autouse=True)
-def clear_tables(db):
-    with db.begin():
-        db.execute(text("TRUNCATE TABLE users RESTART IDENTITY CASCADE;"))
-    yield
 
-@pytest.fixture
-def user(db) -> User:
+@pytest_asyncio.fixture(autouse=True)
+async def clear_table(init_db, db: AsyncSession) -> None:
+    await db.execute(text("TRUNCATE users;"))
+    await db.commit()
+
+
+@pytest_asyncio.fixture
+async def user(db: AsyncSession) -> User:
     user = User(name="John Doe")
     db.add(user)
-    db.commit()
-    db.refresh(user)
+    await db.commit()
+    await db.refresh(user)
     return user
